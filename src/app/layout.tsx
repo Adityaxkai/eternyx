0cb8488a23120e0bbe85e23a41ecbc9a9565a396
NIHAL SAH <eternyxfragrance@gmail.com>
2026-09-26 00:44:51 +0530

    Remove commented Meta Pixel Code
    Removed commented-out Meta Pixel Code from layout.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,6 @@ export default function RootLayout({
     <html lang="en">
       <body>
         
-//<!-- Meta Pixel Code -->
 <script>
 !function(f,b,e,v,n,t,s)
 {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
@@ -41,7 +40,6 @@ fbq('track', 'PageView');
 <noscript><img height="1" width="1" style="display:none"
 src="https://www.facebook.com/tr?id=1050641627454023&ev=PageView&noscript=1"
 /></noscript>
-//<!-- End Meta Pixel Code -->
         <Script src="https://checkout.razorpay.com/v1/checkout.js" strategy="afterInteractive" />
         <StyledJsxRegistry>
           <CartProvider>
